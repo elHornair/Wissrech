984c1e5631b986536683032f40a3f7814eb4c3ab
Alain Horner <alain.horner@liip.ch>
2011-09-25 12:11:43 +0200

    basic command

--- ub_01/ub_01_03.py
+++ ub_01/ub_01_03.py
@@ -1,3 +1,24 @@
-print "hey jude!"
-print "hey dude!"
-print "hey jude!"; print "hey dude!"
+from optparse import OptionParser
+
+def encode(data):
+
+    code = []
+
+    for bit in data:
+	code.append(bit)
+
+    return code
+
+if __name__ == "__main__":
+
+    # init command line parser
+    parser = OptionParser()
+    parser.add_option("-i", "--input", dest="input", help="string consisting of 0 and 1")
+    (options, args) = parser.parse_args()
+
+    print args
+
+    if not options.input:
+        parser.print_help()
+    else:
+        print encode(options.input)
