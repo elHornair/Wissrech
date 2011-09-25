af97d7e498f0d60a1da3ab2333622f3fbf58ce0a
Alain Horner <alain.horner@liip.ch>
2011-09-25 16:03:17 +0200

    added some comments

--- ub_01/ub_01_03.py
+++ ub_01/ub_01_03.py
@@ -19,14 +19,16 @@ def encode(data):
 
         parity_index = 2**parity_exp - 1
 
-        if parity_exp == 0:
+        # define start-index for XOR-sum (p1 and p2 are edge-cases)
+        if parity_exp == 0: #p1
             start_index = 2
-        elif parity_exp == 1:
+        elif parity_exp == 1: #p2
             start_index = 5
             code[parity_index] = code[2]
         else:
             start_index = parity_index + 1
 
+        # build XOR-sum for each parity-bit
         for interval_index in range(start_index, len(code), 2**(parity_exp + 1)):
             for data_index in range(interval_index, interval_index + parity_exp + 1):
                 if len(code) > data_index:
