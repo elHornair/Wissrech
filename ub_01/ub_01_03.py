b065093f60079d546461fd0cb4c29895a5e8c07c
Alain Horner <alain.horner@liip.ch>
2011-09-25 15:58:17 +0200

    n xor n is always 0

--- ub_01/ub_01_03.py
+++ ub_01/ub_01_03.py
@@ -21,13 +21,13 @@ def encode(data):
 
         if parity_exp == 0:
             start_index = 2
-            code[parity_index] = code[start_index] ^ code[start_index]
+            code[parity_index] = 0
         elif parity_exp == 1:
             start_index = 5
             code[parity_index] = code[2]
         else:
             start_index = parity_index + 1
-            code[parity_index] = code[start_index] ^ code[start_index]
+            code[parity_index] = 0
 
         for interval_index in range(start_index, len(code), 2**(parity_exp + 1)):
             for data_index in range(interval_index, interval_index + parity_exp + 1):
